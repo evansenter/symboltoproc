6c335e45f2496ae963a7916bb0046765ed4c9a20
Evan Senter <evansenter@gmail.com>
2009-11-12 12:47:24 -0800

    Added the banner to the main layout, reset CSS file and default expansion code in place.

--- vendor/plugins/railesque/lib/action_view_mixin.rb
+++ vendor/plugins/railesque/lib/action_view_mixin.rb
@@ -2,3 +2,10 @@ ActionView::Helpers::AssetTagHelper.register_javascript_include_default([
   "behaveJS",
   "behaveJS_application"
 ])
+
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion({
+  :defaults => [
+    "reset",
+    "main"
+  ]
+})
