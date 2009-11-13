597f1a3ea4747f1e97e21eb8ee463d0487c3ea6a
Evan Senter <evansenter@gmail.com>
2009-11-12 16:54:43 -0800

    Built gems.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
 # Be sure to restart your server when you modify this file
-require "coderay"
-require "rack/codehighlighter"
-
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
@@ -27,8 +24,6 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem "coderay"
   
-  config.middleware.use Rack::Codehighlighter, :coderay, :element => "pre", :pattern => /\A:::(\w+)\s*\n/
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
