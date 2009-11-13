5b3424656f031793839871934aefaffbe02d720c
Evan Senter <evansenter@gmail.com>
2009-11-12 16:41:10 -0800

    Converted some layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def logged_in_notice
+    admin? ? " You are currently logged in, " + link_to("logout?", logout_path) : ""
+  end
 end
