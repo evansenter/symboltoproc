e5563d1fa71a619c73ecde6ab2686859b53d02cf
Evan Senter <evansenter@gmail.com>
2009-11-12 20:38:03 -0800

    Analytics code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def analytics_code
+    <<-JS
+      <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+      </script>
+      <script type="text/javascript">
+      try {
+      var pageTracker = _gat._getTracker("UA-11576864-1");
+      pageTracker._setDomainName("none");
+      pageTracker._setAllowLinker(true);
+      pageTracker._trackPageview();
+      } catch(err) {}</script>
+    JS
+  end
+  
   def logged_in_notice
     admin? ? " You are currently logged in, " + link_to("logout?", logout_path) : ""
   end
