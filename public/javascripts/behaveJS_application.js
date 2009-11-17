c8163baecfad54debc2afc84f7ab68a5add6439f
Evan Senter <evansenter@gmail.com>
2009-11-16 21:03:27 -0800

    Commented out comments for now, added Smurf for JS minification.

--- public/javascripts/behaveJS_application.js
+++ public/javascripts/behaveJS_application.js
@@ -1,14 +1,11 @@
-// Create Application controller, You can add whatever you like to it, just don't remove it!
 behaveJS.createController("Application", {
  initialize : function() {
-	_.each($$("textarea"), function(element) {
-    CKEDITOR.replace(element);
-  });
-	 }
+  	_.each($$("textarea"), function(element) {
+      CKEDITOR.replace(element);
+    });
+  }
 });
 
-// Add your Behaviors here
 behaveJS.addBehaviors({});
 
-// Add your RemoteActions here
 behaveJS.addRemoteActions({});
